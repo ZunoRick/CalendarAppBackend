1287eccbdaa980d167664dab7dda3b1e745e0972
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-02-20 18:54:54 -0600

    Agregando un middleware

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,16 @@
 
 const { Router } = require('express')
 const { check } = require('express-validator')
-const router = Router()
+const { validarCampos } = require('../middlewares/validar-campos')
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
+
+const router = Router()
 
 router.post('/',
   [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
   ],
   loginUsuario)
 router.post(
@@ -20,6 +23,7 @@ router.post(
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
   ],
   crearUsuario)
 router.get('/renew', revalidarToken)
