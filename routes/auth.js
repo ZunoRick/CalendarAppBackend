95fbc893759ed1da1a7a211f1566018bc1607d3c
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-02-20 18:47:01 -0600

    Agregando authController y validación

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,24 @@
 */
 
 const { Router } = require('express')
+const { check } = require('express-validator')
 const router = Router()
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
 
-router.get('/', (req, res) => {
-  console.log('se require /');
-
-  res.json({
-    ok: true,
-  })
-})
+router.post('/',
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+  ],
+  loginUsuario)
+router.post(
+  '/new', 
+  [ // middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+  ],
+  crearUsuario)
+router.get('/renew', revalidarToken)
 
 module.exports = router
