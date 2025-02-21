b6d967f60bef61fb78a611d8f89467d09904a072
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-02-20 23:45:10 -0600

    Agregando modelo y controlador usaurio

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,40 @@
 const { response } = require('express')
+const bcrypt = require('bcryptjs')
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body
+const crearUsuario = async(req, res = response) => {
+  const { email, password } = req.body
 
-  res.status(201).json({
-    ok: true,
-    msg: 'registro',
-  })
+  try {
+    let usuario = await Usuario.findOne({ email })
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Un usuario existe con ese correo.',
+      })
+    }
+    
+    usuario = new Usuario(req.body)
+
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync()
+    usuario.password = bcrypt.hashSync(password, salt)
+  
+    await usuario.save()
+  
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor comunicarse con el administrador'
+    })
+  }
 }
 
 const loginUsuario = (req, res = response) => {
