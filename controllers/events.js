16aac27f6632843f587530f05554a3fc5cc63484
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-02-23 01:35:31 -0600

    Iniciando CRUD de eventos

---
+++ controllers/events.js
@@ -0,0 +1,54 @@
+const { response } = require('express')
+const Evento = require('../models/Evento')
+
+const getEventos = async(req, res = response) => {
+  return res.status(200).json({
+    ok: true,
+    msg: 'getEventos'
+  })
+}
+
+const crearEvento = async(req, res = response) => {
+  const evento = new Evento(req.body)
+
+  try {
+    evento.user = req.uid
+    const eventoGuardado = await evento.save()
+
+    return res.status(201).json({
+      ok: true,
+      evento: eventoGuardado
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    })
+  }
+}
+
+const actualizarEvento = async(req, res = response) => {
+  console.log(req.body);
+
+  return res.status(200).json({
+    ok: true,
+    msg: 'actualizarEvento'
+  })
+}
+
+const eliminarEvento = async(req, res = response) => {
+  console.log(req.body);
+
+  return res.status(200).json({
+    ok: true,
+    msg: 'eliminarEvento'
+  })
+}
+
+module.exports = {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento
+}
---
+++ routes/events.js
@@ -0,0 +1,37 @@
+/*
+  Rutas de eventos / events
+  host + /apí/events
+*/
+
+const { Router } = require('express')
+const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos')
+const { validarJWT } = require('../middlewares/validar-jwt')
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events')
+const { isDate } = require('../helpers/isDate')
+
+const router = Router()
+
+router.use(validarJWT)
+
+// Obtener eventos
+router.get('/', getEventos)
+
+// Crear un evento
+router.post('/',
+  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos
+  ],
+  crearEvento
+)
+
+// Actualizar un evento
+router.put('/:id', actualizarEvento)
+
+// Borrar evento
+router.delete('/:id', eliminarEvento)
+
+module.exports = router
